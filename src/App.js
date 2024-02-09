e2a45f123ab404a206597af6f581100a8a77238a
ggang89 <ggang89u@gmail.com>
2024-02-09 13:21:38 +0900

    컴포넌트 분리해서 함수로 export해서 연결해줌

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 
 import './App.css';
-import { useState } from 'react';
+import Counter2 from './component/counter/counter';
+
 // ToDo : useState만 중괄호 쓰는 이유
+import Counter from './component/counter';
 
 function App() {
 //js코드 쓰는 자리
-const [sugar, setSugar] = useState(0);
-// count :변하는 값,변수 명  / setCount :변경함수 /useState(초기값)
-const handleAdd =()=>{
-  setSugar(sugar +1);
-}
-const handleMinus =()=>{
-  setSugar(sugar -1);
-}
+
   return (
     <div>
 
@@ -24,14 +19,9 @@ const handleMinus =()=>{
      
     </header>
 
-    <div className='container'>
-      <button onClick={handleMinus}>-1</button>
-      <p className="counter_text">{sugar}</p>
-      {/* 변수를 사용하려면 중괄호를 한다 */}
-      <button onClick={handleAdd}>+1</button>
-
-    </div>
-
+  
+<Counter/>
+<Counter2/>
     </div>
   );
 }
---
+++ src/component/counter/index.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Counter(){
+  const [sugar, setSugar] = useState(0);
+  // count :변하는 값,변수 명  / setCount :변경함수 /useState(초기값)
+  const handleAdd =()=>{
+    setSugar(sugar +1);
+  }
+  const handleMinus =()=>{
+    setSugar(sugar -1);
+  }
+
+
+  return(
+<div className='container'>
+      <button onClick={handleMinus}>-1</button>
+      <p className="counter_text">{sugar}</p>
+      {/* 변수를 사용하려면 중괄호를 한다 */}
+      <button onClick={handleAdd}>+1</button>
+
+    </div>
+  );
+}
