1522992dc6fe9b5a9eda339f48f46617f1f7a174
ggang89 <ggang89u@gmail.com>
2024-02-09 12:53:39 +0900

    useState 연결하고, count변수를 만들어서 연결해줌

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import { useState } from 'react';
+// ToDo : useState만 중괄호 쓰는 이유
 
 function App() {
+//js코드 쓰는 자리
+const [count, setCount] = useState(0);
+// count :변하는 값  / setCount :변경함수 /useState(초기값)
   return (
     <div>
-      
+
     <header>
      
         <h1 className='headerText'>
@@ -13,7 +18,13 @@ function App() {
      
     </header>
 
-    
+    <div className='container'>
+      <button>-1</button>
+      <p className="counter_text">{count}</p>
+      {/* 변수를 사용하려면 중괄호를 한다 */}
+      <button>+1</button>
+
+    </div>
 
     </div>
   );
